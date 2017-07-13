e6f8bc43436e2442ba92ac1984b70098d8699c46
aitva <arod.louis@gmail.com>
2017-07-13 14:54:09 +0200

    Indent and escape names.

--- music_db.sql
+++ music_db.sql
@@ -1,42 +1,42 @@
 # music_db is a database to store all we like about music.
 
 # DEBUG: drop db if exists.
-DROP DATABASE IF EXISTS music_db;
+DROP DATABASE IF EXISTS `music_db`;
 
-CREATE DATABASE music_db;
+CREATE DATABASE `music_db`;
 
-USE music_db;
+USE `music_db`;
 
 CREATE TABLE `group` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    start DATE NOT NULL,
-    end DATE,
-    origin VARCHAR(64)
+    `id`        INT AUTO_INCREMENT PRIMARY KEY,
+    `name`      VARCHAR(64) NOT NULL,
+    `start`     DATE NOT NULL,
+    `end`       DATE,
+    `origin`    VARCHAR(64)
 );
 
 CREATE TABLE `member` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(64) NOT NULL,
-    lastname VARCHAR(128) NOT NULL,
-    nickname VARCHAR(64),
-    role VARCHAR(64) NOT NULL,
-    birth DATE NOT NULL,
-    death DATE
+    `id`        INT AUTO_INCREMENT PRIMARY KEY,
+    `firstname` VARCHAR(64) NOT NULL,
+    `lastname`  VARCHAR(128) NOT NULL,
+    `nickname`  VARCHAR(64),
+    `role`      VARCHAR(64) NOT NULL,
+    `birth`     DATE NOT NULL,
+    `death`     DATE
 );
 
 CREATE TABLE `award` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL
+    `id`    INT AUTO_INCREMENT PRIMARY KEY,
+    `name`  VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE `album` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    `date` DATE NOT NULL,
-    label VARCHAR(128) NOT NULL,
-    genre VARCHAR(128),
-    sales INT
+    `id`    INT AUTO_INCREMENT PRIMARY KEY,
+    `name`  VARCHAR(64) NOT NULL,
+    `date`  DATE NOT NULL,
+    `label` VARCHAR(128) NOT NULL,
+    `genre` VARCHAR(128),
+    `sales` INT
 );
 
 # Ajoute une colone au tableau album.
